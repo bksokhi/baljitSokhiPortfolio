f3c674bc930cc9ff15323ee9a04b23d9329b153f
Baljit <sokhibaljit@gmail.com>
2020-10-15 00:36:37 -0400

    scroll animation

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
+
+
+AOS.init();
+ 
+
+
     $(".burgerOpen").on("click", function () {
         $(".navMenu").css({ width: "200px", padding: "20px" });
+        $(".burgerOpen").css({ display: "none" });
     });
 
     $(".burgerClose").on("click", function () {
         $(".navMenu").css({ width: "0px", padding: "0px" });
+        $(".burgerOpen").css({ display: "block" });
     });
+
+    // $(window).on("scroll", function () {
+    //     $(".banner").css({ "background": "green"});
+    //     // $(".burgerOpen").css({ color: "white" });
+    // });
+
 });
-
-
-
-
-// openNav = () => {
-//     document.getElementsByClassName("navMenu").style.width = "200px";
-//     document.getElementsByClassName("navMenu").style.padding = "20px";
-// }
-
-// closeNav = () => {
-//     document.getElementsByClassName("navMenu").style.width = "0";
-//     document.getElementsByClassName("navMenu").style.padding = "0";
-// }
